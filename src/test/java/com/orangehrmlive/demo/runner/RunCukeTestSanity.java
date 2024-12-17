95a7b5c20935bbdb4983d0674b9bc9fd571240f9
Isabela1706 <id1317238@gmail.com>
2024-12-17 12:17:34 +0000

    Done1

---
+++ src/test/java/com/orangehrmlive/demo/runner/RunCukeTestSanity.java
@@ -0,0 +1,29 @@
+package com.orangehrmlive.demo.runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+/**
+ * Created by Jay Vaghani
+ */
+
+
+@CucumberOptions(
+        features = "src/test/java/resources/features",
+        glue = "com/orangehrmlive/demo/steps",
+        plugin = {"html:target/cucumber-reports/cucumber.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+        "json:target/cucumber-reports/cucumber.json"},
+        tags = "@sanity"
+
+)
+public class RunCukeTestSanity extends AbstractTestNGCucumberTests {
+
+
+
+
+
+
+
+
+}
